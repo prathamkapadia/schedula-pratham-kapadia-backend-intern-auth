1b981ef0fd330f637d1cffb014a5ae705168f250
prathamkapadia <prathamkapadia19@gmail.com>
2026-06-10 13:27:08 +0530

    debug env var

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('DATABASE_URL:', process.env.DATABASE_URL ? 'FOUND ✅' : 'MISSING ❌');
+  
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
